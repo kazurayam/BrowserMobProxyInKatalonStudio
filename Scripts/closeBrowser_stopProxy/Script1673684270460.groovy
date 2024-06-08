e3a92df4893fe460609406bed44d67b108b32831
kazurayam <kazuaki.urayama@gmail.com>
2024-06-09 07:15:07 +0900

    comments on the Test Cases

--- Scripts/closeBrowser_stopProxy/Script1673684270460.groovy
+++ Scripts/closeBrowser_stopProxy/Script1673684270460.groovy
@@ -4,17 +4,19 @@ import groovy.json.JsonOutput
 import internal.GlobalVariable
 import net.lightbody.bmp.core.har.Har;
 
-
+// close the browser
 WebUI.closeBrowser()
 
+// get the HAR content
 Har har = GlobalVariable.proxy.getHar()
 
 StringWriter sw = new StringWriter()
 har.writeTo(sw)
 
+// save the json into a file
 def pp = JsonOutput.prettyPrint(sw.toString())
 File f = new File("sample.har")
 f.text = pp
 
-
+// terminate the proxy process
 GlobalVariable.proxy.stop()
