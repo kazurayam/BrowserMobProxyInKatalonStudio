0830cae489564e3643480f6ced802536ab82fabc
kazurayam <kazuaki.urayama@gmail.com>
2024-07-19 23:24:13 +0900

    #18 Test Cases/squeeze_har_using_JsonPath now works

--- Scripts/squeeze_har_using_JsonPath/Script1721397043651.groovy
+++ Scripts/squeeze_har_using_JsonPath/Script1721397043651.groovy
@@ -10,11 +10,21 @@ import com.kms.katalon.core.configuration.RunConfiguration
 
 import groovy.json.JsonOutput
 
+// Test Cases/squeeze_har_using_JsonPath
 /**
- * This Test Case post-processes the '<projectDir>/sample.har' JSON file
- * using JayWay JsonPath library.
- *
- * See https://www.baeldung.com/guide-to-jayway-jsonpath
+ * This script reads a HAR file, filter the array entries that match my interest,
+ * write the result into a new json file.
+ * 
+ * The resulting json will be far smaller in byte size than the original HAR file. 
+ * e.g, less than 5%.
+ * The resulting json is compact because unnecessary array entries are chomped off.
+ * The resulting json is informative, good to share with others.
+ * 
+ * This script makes use of Jayway JsonPath library in depth.
+ * For more information about JsonPath, see 
+ * - https://www.baeldung.com/guide-to-jayway-jsonpath
+ * 
+ * @author kazurayam
  */
 Path projectDir = Paths.get(RunConfiguration.getProjectDir())
 Path har = projectDir.resolve("work/sample.har")
@@ -33,22 +43,26 @@ def reconstructed = ["log":[
 						"version": null,
 						"creator": null,
 						"pages": null, 
-						"entries": squeezed
+						"entries": squeezed,
+						"comment": ""
 						]]
 
+// write the squeezed JSON text into file
 String juice = JsonOutput.toJson(reconstructed)
 String pretty = JsonOutput.prettyPrint(juice)
 Path result = projectDir.resolve("work/squeezed.json")
 Files.writeString(result, pretty)
 
-//
+// show size of the files
 int hl = har.toFile().length()
 String hls = String.format("%,11d", hl)
 int ql = result.toFile().length()
 String qls = String.format("%,11d", ql)
 int percent = Math.floor(ql * 100 / hl)
+
 println "HAR length      = ${hls}"
 println "squeezed length = ${qls} (${percent}%)"
 
-// we expect the result file is far smaller than the source HAR file; less than 5%
+// we expect the result file is far smaller than the source HAR file;
+// the squeezed file should be less than 5% of the source
 assert percent < 5
